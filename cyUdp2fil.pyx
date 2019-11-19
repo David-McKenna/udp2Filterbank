e1fe132f323fdc4d7528b209b82055f8eebf1515
David McKenna <mckennd2@tcd.ie>
2019-11-19 12:27:23 +0000

    To squish:

--- cyUdp2fil.pyx
+++ cyUdp2fil.pyx
@@ -166,10 +166,10 @@ cpdef void readFile(char* fileLoc, int threadCount, long long readStart, long lo
 	# fileData is free'd in this function
 	processData(fileData, threadCount, packetCount, stokesIT, stokesVT, timeDecimation, freqDecimation, outputF, outputF2)
 
-#cdef tuple processData(DTYPE_t_1* fileData, long packetCount, unsigned char stokesIT, unsigned char stokesVT, int timeDecimation, int freqDecimation, char* outputLoc):
 cdef void processData(DTYPE_t_1* fileData, int threadCount, long packetCount, unsigned char stokesIT, unsigned char stokesVT, int timeDecimation, int freqDecimation, char* outputLoc, char* outputLoc2):
 
 	# Initialise all variables.
+	cdef func 
 	cdef object t1, t2
 
 	cdef unsigned char Xr, Xi, Yr, Yi
@@ -185,9 +185,7 @@ cdef void processData(DTYPE_t_1* fileData, int threadCount, long packetCount, un
 	cdef DTYPE_t_2 *hannWindow = <DTYPE_t_2*>malloc(sizeof(DTYPE_t_2) * freqDecimation)
 	cdef DTYPE_t_2 pi = np.pi
 
-	cdef func 
-
-	cdef long i, iSet
+	cdef long i, iSet, baseOffset, beamletIdx, __
 	cdef long udpPacketLength = 7824
 	cdef long udpHeaderLength = 16
 	cdef long timeIdx = 0
@@ -195,11 +193,20 @@ cdef void processData(DTYPE_t_1* fileData, int threadCount, long packetCount, un
 
 	cdef long long dataLength = int(packetCount * (beamletCount) * (scans / timeDecimation) * (stokesIT + stokesVT)) # freqDecimation has a 1:1 transfer between time/freq, so no effect on output size.
 
+	cdef fftwf_complex *inVarX
+	cdef fftwf_complex *outVarX
+	cdef fftwf_plan fftPlanX
+
+	cdef fftwf_complex *inVarY
+	cdef fftwf_complex *outVarY
+	cdef fftwf_plan fftPlanY
 
 	printf("Output expected to be %lld bytes long, from %ld packets and %d modes.\n", dataLength * sizeof(DTYPE_t_2), packetCount, stokesIT + stokesVT)
 
 
-	# Initialise memory for our data
+	# Initialise memory for our data, setup memviews
+	# Cython requires empties be setup if we are going to define a variable,
+	# 	so file 1 element arrays for the unused shape.
 	structuredFileData = np.zeros((beamletCount, packetCount * scans,  4), dtype = DTYPE_1)
 
 	if stokesVT + stokesIT == 1:
@@ -223,49 +230,35 @@ cdef void processData(DTYPE_t_1* fileData, int threadCount, long packetCount, un
 
 
 
-	# Initialise FFTW, WARNING, THIS IS NOT THREAD SAFE, DO NOT ATTEMPT TO OMP/PRANGE YOUR WAY TO PARALLELISM AGAIN
+	# Initialise FFTW operations and memory 
 	if freqDecimation > 1:
-		fftwf_plan_with_nthreads(threadCount)
+		# Spread each fft over only 1 thread, but thread safe
+		fftwf_plan_with_nthreads(1)
 
 	cdef fftwf_complex **inVarXArr = <fftwf_complex**>malloc(beamletCount * sizeof(fftwf_complex *))
 	cdef fftwf_complex **outVarXArr = <fftwf_complex**>malloc(beamletCount * sizeof(fftwf_complex *))
-	cdef fftwf_plan *fftPlanXArr = <fftwf_plan*>malloc(beamletCount * sizeof(fftwf_plan *))
+	cdef fftwf_plan *fftPlanXArr = <fftwf_plan*> malloc(beamletCount * sizeof(fftwf_plan *))
 
 	for i in range(beamletCount):
 		inVarXArr[i] = <fftwf_complex*> fftwf_malloc(sizeof(fftwf_complex) * freqDecimation);
 		outVarXArr[i] = <fftwf_complex*> fftwf_malloc(sizeof(fftwf_complex) * freqDecimation);
-		fftPlanXArr[i] =  <fftwf_plan>fftwf_plan_dft_1d(freqDecimation, inVarXArr[i], outVarXArr[i], -1, FFTW_ESTIMATE)
+		fftPlanXArr[i] =  <fftwf_plan> fftwf_plan_dft_1d(freqDecimation, inVarXArr[i], outVarXArr[i], -1, FFTW_ESTIMATE)
 
 
 	cdef fftwf_complex **inVarYArr = <fftwf_complex**>malloc(beamletCount * sizeof(fftwf_complex *))
 	cdef fftwf_complex **outVarYArr =<fftwf_complex**> malloc(beamletCount * sizeof(fftwf_complex *))
-	cdef fftwf_plan *fftPlanYArr =  <fftwf_plan*>malloc(beamletCount * sizeof(fftwf_plan *))
+	cdef fftwf_plan *fftPlanYArr =  <fftwf_plan*> malloc(beamletCount * sizeof(fftwf_plan *))
 
 	for i in range(beamletCount):
 		inVarYArr[i] = <fftwf_complex*> fftwf_malloc(sizeof(fftwf_complex) * freqDecimation);
 		outVarYArr[i] = <fftwf_complex*> fftwf_malloc(sizeof(fftwf_complex) * freqDecimation);
-		fftPlanYArr[i] = <fftwf_plan>fftwf_plan_dft_1d(freqDecimation, inVarYArr[i], outVarYArr[i], -1, FFTW_ESTIMATE)
+		fftPlanYArr[i] = <fftwf_plan> fftwf_plan_dft_1d(freqDecimation, inVarYArr[i], outVarYArr[i], -1, FFTW_ESTIMATE)
 
-
-	cdef fftwf_complex *inVarX
-	cdef fftwf_complex *outVarX
-	cdef fftwf_plan fftPlanX
-
-	cdef fftwf_complex *inVarY
-	cdef fftwf_complex *outVarY
-	cdef fftwf_plan fftPlanY
-	"""
-	cdef fftwf_complex *inVarY = <fftwf_complex*> fftwf_malloc(sizeof(fftwf_complex) * freqDecimation);
-	cdef fftwf_complex *outVarY = <fftwf_complex*> fftwf_malloc(sizeof(fftwf_complex) * freqDecimation);
-	cdef fftwf_plan fftPlanY = fftwf_plan_dft_1d(freqDecimation, inVarY, outVarY, -1, FFTW_ESTIMATE)
-	"""
 
 	# Change the memory from time continuous to beam continuous
-	# Easy optimisation: do this when dealing with the rest of the memory operations
 	# This is simply to make my life easier while creating this script.
 	printf("Restructuring the dataset in memory...\n")
 
-	cdef long baseOffset, beamletIdx, __
 	for i in prange(packetCount, nogil = True, schedule = 'guided', num_threads = threadCount):
 	#for i in range(packetCount):
 		baseOffset = udpPacketLength * i + udpHeaderLength
@@ -288,13 +281,16 @@ cdef void processData(DTYPE_t_1* fileData, int threadCount, long packetCount, un
 	for i in range(freqDecimation):
 		hannWindow[i] = pow(sin(pi * i / freqDecimation), 2)
 
+
+	# Being actually processing data
 	if stokesVT and stokesIT:
 		printf("Processing StoKes I and Stokes V...\n")
 		t1 = time.time()
 
+
+		# Handle the frequency/time tradeoff
 		if freqDecimation > 1:
 			for j in prange(beamletCount, nogil = True, schedule = 'guided', num_threads = threadCount):
-			#for j in range(beamletCount):
 				filterbankIdx = 0
 				filterbankLim = freqDecimation - 1
 				timeIdx = 0
@@ -308,6 +304,7 @@ cdef void processData(DTYPE_t_1* fileData, int threadCount, long packetCount, un
 
 
 				for i in range(timeSteps):
+					# Data structure reminder
 					#Xr = structuredFileData_view[j][i][0]
 					#Xi = structuredFileData_view[j][i][1]
 					#Yr = structuredFileData_view[j][i][2]
@@ -324,6 +321,7 @@ cdef void processData(DTYPE_t_1* fileData, int threadCount, long packetCount, un
 						fftwf_execute(fftPlanY)
 
 						for l in range(fftOffset):
+							# Stored values are frequency reversed, FFT shifted
 							offsetIdx = l + fftOffset
 							idx1 = fftOffset - 1 - l
 							idx2 = freqDecimation - 1 - l
@@ -337,6 +335,7 @@ cdef void processData(DTYPE_t_1* fileData, int threadCount, long packetCount, un
 					else:
 						filterbankIdx = filterbankIdx + 1
 
+				# Cleanup fftw memory objects
 				fftwf_destroy_plan(fftPlanX)
 				fftwf_destroy_plan(fftPlanY)
 				fftwf_free(inVarX)
@@ -345,14 +344,17 @@ cdef void processData(DTYPE_t_1* fileData, int threadCount, long packetCount, un
 				fftwf_free(outVarY)
 
 		else:
+			# Just process the Stokes values if we aren't doing a frequency trade off
 			for j in prange(beamletCount, nogil = True, schedule = 'guided', num_threads = threadCount):
 			#for j in range(beamletCount):
 				for i in range(timeSteps):
 					stokesDual_view[0][i][j] = stokesI(structuredFileData_view[j][i][0], structuredFileData_view[j][i][1], structuredFileData_view[j][i][2], structuredFileData_view[j][i][3])
 					stokesDual_view[1][i][j] = stokesV(structuredFileData_view[j][i][0], structuredFileData_view[j][i][1], structuredFileData_view[j][i][2], structuredFileData_view[j][i][3])
 
-
+		# Account for reduced time steps from frequency trade offs
 		timeSteps /= freqDecimation
+
+		# Sum over time if we are decimating the data
 		if timeDecimation > 1:
 			for j in prange(beamletCount, nogil = True, schedule = 'guided', num_threads = threadCount):
 			#for j in range(beamletCount):
@@ -375,10 +377,12 @@ cdef void processData(DTYPE_t_1* fileData, int threadCount, long packetCount, un
 		print("This took {:.2f} seconds, each sample taking {:f} seconds to process.".format(t2 - t1, (t2 - t1) / stokesDualData.size / 2))
 
 		t1 = time.time()
+		# Write the results to disk
 		writeData(stokesDualOut_view, dataLength, &outputLoc[0], &outputLoc2[0])
 
 	else:
 
+		# Select our Stokes function
 		if stokesIT:
 			stokesFunc = stokesI
 			stokesFFunc = stokesIf
@@ -393,6 +397,7 @@ cdef void processData(DTYPE_t_1* fileData, int threadCount, long packetCount, un
 
 		t1 = time.time()
 
+		# Handle the frequency/time tradeoff
 		if freqDecimation > 1:
 			for j in prange(beamletCount, nogil = True, schedule = 'guided', num_threads = threadCount):
 			#for j in range(beamletCount):
@@ -408,6 +413,7 @@ cdef void processData(DTYPE_t_1* fileData, int threadCount, long packetCount, un
 				fftPlanY = fftPlanYArr[j]
 
 				for i in range(timeSteps):
+					# Data structure reminder
 					#Xr = structuredFileData_view[j][i][0]
 					#Xi = structuredFileData_view[j][i][1]
 					#Yr = structuredFileData_view[j][i][2]
@@ -424,6 +430,7 @@ cdef void processData(DTYPE_t_1* fileData, int threadCount, long packetCount, un
 						fftwf_execute(fftPlanY)
 
 						for l in range(fftOffset):
+							# Stored values are frequency reversed, FFT shifted
 							offsetIdx = l + fftOffset
 							idx1 = fftOffset - 1 - l
 							idx2 = freqDecimation - 1 - l
@@ -435,7 +442,7 @@ cdef void processData(DTYPE_t_1* fileData, int threadCount, long packetCount, un
 					else:
 						filterbankIdx = filterbankIdx + 1
 
-
+				# Cleanup fftw memory objects on a per-beam basis
 				fftwf_destroy_plan(fftPlanX)
 				fftwf_destroy_plan(fftPlanY)
 				fftwf_free(inVarX)
@@ -443,15 +450,19 @@ cdef void processData(DTYPE_t_1* fileData, int threadCount, long packetCount, un
 				fftwf_free(outVarX)
 				fftwf_free(outVarY)
 
-
+			fftwf_cleanup_threads()
 		else:
+			# Just process the Stokes value if we aren't doing a frequency trade off
 			for j in prange(beamletCount, nogil = True, schedule = 'guided', num_threads = threadCount):
 			#for j in range(beamletCount):
 				for i in range(timeSteps):
 					stokesSingle_view[i][j] = stokesFunc(structuredFileData_view[j][i][0], structuredFileData_view[j][i][1], structuredFileData_view[j][i][2], structuredFileData_view[j][i][3])
 
 
+		# Account for reduced time steps from frequency trade offs
 		timeSteps /= freqDecimation
+
+		# Sum over time if we are decimating the data
 		if timeDecimation > 1:
 			for j in prange(beamletCount, nogil = True, schedule = 'guided', num_threads = threadCount):
 			#for j in range(beamletCount):
@@ -473,11 +484,19 @@ cdef void processData(DTYPE_t_1* fileData, int threadCount, long packetCount, un
 		print("This took {:.2f} seconds, each sample taking {:} seconds to process.".format(t2 - t1, (t2 - t1) / stokesSingleData.size))
 
 		t1 = time.time()
+		# Write the results to disk.
 		writeDataShrunk(stokesSingleOut_view, dataLength, &outputLoc[0])
 
 	t2 = time.time()
 	print("This took {:.2f} seconds, giving a write speed of {:.2f}MB/s".format(t2 - t1, dataLength * 4 / 1024 / 1024 / (t2 - t1)))
 
 
-	fftwf_cleanup_threads()
+	# Cleanup the allocated memory / fftw remnants
+	# Memviews are handled by the Python GC (unfortunatly, can't empty them early)
 	free(hannWindow)
+	free(inVarXArr)
+	free(inVarYArr)
+	free(outVarXArr)
+	free(outVarYArr)
+	free(fftPlanXArr)
+	free(fftPlanYArr)
